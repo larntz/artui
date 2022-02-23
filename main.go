8df94bc67f883344a9d847650d2dea60ecfe53aa
larntz <luke@blue42.net>
2022-02-23 10:05:43 -0500

    part way through adding cobra and viper

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-
-	"github.com/larntz/artui/argo"
-	"github.com/larntz/artui/ui"
-)
+import "github.com/larntz/artui/cobra/cmd"
 
 func main() {
-	// setup logging
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		fmt.Println("fatal:", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-	log.Println("Application Start")
-	apps := argo.GetApplications()
-
-	log.Println("Got Applications")
-
-	// start application
-	log.Println("UI Start")
-	p := tea.NewProgram(ui.InitializeModel(apps), tea.WithAltScreen(), tea.WithMouseAllMotion()) // tea.WithMouseCellMotion(),
-	if err := p.Start(); err != nil {
-		panic(err)
-	}
-	log.Println("Application Exit")
+	cmd.Execute()
 }
