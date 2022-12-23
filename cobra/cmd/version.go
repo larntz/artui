4f1a2f97b3802b52abe9beb0be2b0445bbb1c754
Luke Arntz <luke.arntz@swimlane.com>
2022-12-23 14:30:19 -0500

    update action, version.go

--- cobra/cmd/version.go
+++ cobra/cmd/version.go
@@ -11,13 +11,15 @@ func init() {
 	rootCmd.AddCommand(versionCmd)
 }
 
-var version = "asdf"
+var version string
+var date string
+var hash string
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print artui version.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Artui version %s\n", version)
+		fmt.Printf("Artui version: %s, build_date: %s, commit_hash: %s\n", version, date, hash)
 		os.Exit(0)
 	},
 }
